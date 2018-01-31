b29f2b554e44ed3c6098e275e1e4a93ec72d7696
Krystian Bajno <33201769+krystianbajno@users.noreply.github.com>
2018-01-31 15:52:29 +0100

    added utility
    checkProxy.sh

---
+++ checkCountry.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Usage: ./checkCountry.sh <proxylist> optional: <searched country name>
+
+if [[ "$#" -lt 1 ]] || [[ "$#" -gt 2 ]];then
+echo "Usage: ./checkCountry.sh <proxylist> optional: <searched country name>"
+else
+which geoiplookup
+geoip=$?
+filename="$1"
+while read -r line
+do	
+   host=$(echo $line | cut -d ":" -f 1 | cut -d '(' -f 1 )
+    if [[ $geoip == 0 ]]; then
+    	geoiplookup  $host | grep -i "$2" -A 3 -B 1 && echo -e "$host\n";
+    else
+    	whois $host | grep -i "$2" && echo -e "$host\n";
+    fi
+done < "$filename"
+fi
