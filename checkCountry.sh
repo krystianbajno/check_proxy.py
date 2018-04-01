6e4564b52530bc276195159b6763ef56b32fdbcf
Krystian Bajno <krystian.bajno@gmail.com>
2018-04-01 03:12:56 +0200

    Update checkCountry.sh

--- checkCountry.sh
+++ checkCountry.sh
@@ -9,7 +9,8 @@ geoip=$?
 filename="$1"
 while read -r line
 do	
-   host=$(echo $line | cut -d ":" -f 1 | cut -d '(' -f 1 )
+   host=$(echo $line | cut -d ":" -f 1 )
+   port=$(echo $line | cut -d ":" -f 2 )
     if [[ $geoip == 0 ]]; then
     	geoiplookup  $host | grep -i "$2" -A 3 -B 1 && echo -e "$host\n";
     else
