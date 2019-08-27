c97e5ddf96e735216ee5ca248732584761d1f25d
krystian <krystian.bajno@liteweb.io>
2019-08-27 12:27:24 +0200

    minified only

--- libs/Files/Saver.py
+++
@@ -1,9 +0,0 @@
-class Saver(object):
-    def __init__(self, handle):
-        self.handle = handle
-
-    def save(self, line):
-        self.handle.write(line+ "\n")
-        return False
-    def close(self):
-        self.handle.close()
--- libs/Misc/HelpUtils.py
+++
@@ -1,5 +0,0 @@
-def printHelp():
-    print("Usage: check_proxy.py <proxy-list> <output-list> <number-of-threads>")
-
-def partition (lst, n ):
-    return [ lst[i::n] for i in range(n) ]
