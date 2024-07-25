6f1147b33efe831add819c4a6d9bc9690e9db325
Artideusz <materac2000@gmail.com>
2024-07-25 15:21:22 +0200

    SOCKS + Contributions in banner

--- core/colors.py
+++ core/colors.py
@@ -3,3 +3,5 @@ class Colors:
     GREEN = '\033[0;32m'
     YELLOW = '\033[0;33m'
     CYAN = '\033[0;36m'
+    VIOLET = '\033[38;5;105m'
+    BOLD = '\033[1m'
--- core/proxy_checker/socks_proxy_checker.py
+++ core/proxy_checker/socks_proxy_checker.py
@@ -8,19 +8,38 @@ class SocksProxyChecker(ProxyChecker):
             self.on_check(proxy)
             session = requests.Session()
 
-            proxies = {
+            proxies = [
+                {
+                "version": "socks5",
                 "http": f"socks5://{proxy}",
                 "https": f"socks5://{proxy}",
+                },
+                {
+                "version": "socks4",
                 "http": f"socks4://{proxy}",
                 "https": f"socks4://{proxy}",
+                },
+                {
+                "version": "https",
                 "http": proxy,
                 "https": proxy
-            }
+                }
+            ]
 
-            response = session.get(self.INTERROGATOR_URL, proxies=proxies, timeout=8)
+            response = None
+            current_version = None
+
+            for proxy_dict in proxies:
+                try:
+                    current_version = proxy_dict["version"]
+                    response = session.get(self.INTERROGATOR_URL, proxies=proxy_dict, timeout=8)
+                    break
+                except Exception as e:
+                    continue
+
        
-            if self.is_response_not_tampered(response):
-                self.on_proxy_found(proxy)
+            if response is not None and self.is_response_not_tampered(response):
+                self.on_proxy_found(current_version + "://" + proxy)
 
         except requests.RequestException:
             pass
